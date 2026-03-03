514e0b2a4ea79e034123c1dbf639d908cefb2111
dev <devnull@example.com>
2026-03-03 20:21:35 +1300

    Add Student CRUD routes and templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -72,5 +73,59 @@ def create_app(test_config=None):
         
         schools = query.all()
         return render_template('info.html', schools=schools)
+
+    @app.route('/students')
+    def students():
+        from .models import Student
+        students = Student.query.order_by(Student.stu_name).all()
+        return render_template('students.html', students=students)
+
+    @app.route('/students/new', methods=['GET', 'POST'])
+    def new_student():
+        from .models import Student
+        if request.method == 'POST':
+            name = request.form.get('stu_name')
+            birth = request.form.get('stu_birth')
+            gender = request.form.get('stu_gender')
+            email = request.form.get('stu_email')
+            phone = request.form.get('stu_phone')
+            dob = None
+            if birth:
+                try:
+                    dob = datetime.date.fromisoformat(birth)
+                except Exception:
+                    dob = None
+            s = Student(stu_name=name, stu_birth=dob, stu_gender=gender, stu_email=email, stu_phone=phone)
+            db.session.add(s)
+            db.session.commit()
+            return redirect(url_for('students'))
+        return render_template('student_form.html', student=None)
+
+    @app.route('/students/<int:stu_id>/edit', methods=['GET', 'POST'])
+    def edit_student(stu_id):
+        from .models import Student
+        s = Student.query.get_or_404(stu_id)
+        if request.method == 'POST':
+            s.stu_name = request.form.get('stu_name')
+            birth = request.form.get('stu_birth')
+            if birth:
+                try:
+                    s.stu_birth = datetime.date.fromisoformat(birth)
+                except Exception:
+                    s.stu_birth = None
+            s.stu_gender = request.form.get('stu_gender')
+            s.stu_email = request.form.get('stu_email')
+            s.stu_phone = request.form.get('stu_phone')
+            db.session.commit()
+            return redirect(url_for('students'))
+        return render_template('student_form.html', student=s)
+
+    @app.route('/students/<int:stu_id>/delete', methods=['POST'])
+    def delete_student(stu_id):
+        from .models import Student
+        s = Student.query.get_or_404(stu_id)
+        db.session.delete(s)
+        db.session.commit()
+        return redirect(url_for('students'))
 
     return app
